8ce44bcd0b1a7b7aad2de7ee90907f549c85012e
Monal Mahajan <monal.mahajan@hotmail.com>
2020-04-26 15:23:05 +0400

    Changed textbox view of Enter Course Title and Number to combobox

--- Purdue Books/Login-Designs/ViewControllers/ListingViewController.swift
+++ Purdue Books/Login-Designs/ViewControllers/ListingViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 import AlamofireImage
 import Parse
+import DropDown
 
 class ListingViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var descriptionOfTextbook: UITextField!
-    @IBOutlet weak var courseNumber: UITextField!
+    @IBOutlet weak var courseNumberButton: UIButton!
+    @IBOutlet weak var courseTitleButton: UIButton!
     @IBOutlet weak var titleTextbook: UITextField!
     @IBOutlet weak var picture: UIImageView!
-    @IBOutlet weak var course: UITextField!
+    
+    var titles = [String]()
+    var numbers = [String]()
+    var titles_read = [PFObject]()
+    var numbers_read = [PFObject]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadTitleData()
         // Do any additional setup after loading the view.
     }
     
@@ -28,8 +35,8 @@ class ListingViewController: UIViewController, UIImagePickerControllerDelegate, 
         listingTB["Author"] = PFUser.current()!
         listingTB["Name"] = titleTextbook.text
         listingTB["Description"] = descriptionOfTextbook.text
-        listingTB["CouseNumber"] = courseNumber.text
-        listingTB["Course"] = course.text
+        listingTB["CouseNumber"] = courseNumberButton.currentTitle
+        listingTB["Course"] = courseTitleButton.currentTitle
         let imageData = picture.image!.pngData()
         let file = PFFileObject(name: "image.png", data: imageData! )
         listingTB["image"] = file
@@ -44,12 +51,86 @@ class ListingViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     @IBAction func onClear(_ sender: Any) {
-        courseNumber.text = nil
+        courseNumberButton.setTitle("Choose Course Number", for: .normal)
         titleTextbook.text = nil
         picture = nil
-        course.text = nil
+        courseTitleButton.setTitle("Choose Course Title", for: .normal)
         descriptionOfTextbook.text = nil
     }
+    @objc func loadTitleData() {
+        titles.removeAll()
+        let query = PFQuery(className: "Courses");
+        query.order(byDescending: "createdAt")
+        query.findObjectsInBackground { (posts: [PFObject]?, error: Error?) in
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let posts = posts {
+              print("Successfully retrieved \(posts.count) posts.")
+            self.titles_read = posts
+           }
+        }
+        for post in titles_read {
+            titles.append((post["Name"] as? String)!)
+        }
+        loadNumberData()
+    }
+    @objc func loadNumberData() {
+        numbers.removeAll()
+        let query = PFQuery(className: "Numbers");
+        query.order(byAscending: "Number")
+        query.findObjectsInBackground { (posts: [PFObject]?, error: Error?) in
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let posts = posts {
+              print("Successfully retrieved \(posts.count) posts.")
+            self.numbers_read = posts
+           }
+        }
+        for post in numbers_read {
+            let number_string = post["Number"] as? String
+            let name_string = post["Name"] as? String
+            if (name_string == courseTitleButton.currentTitle) {
+                numbers.append(number_string!)
+            }
+        }
+    }
+    @objc func loadTitle() {
+//        print("Executes")
+        let dropDown = DropDown()
+        dropDown.anchorView = courseTitleButton
+        dropDown.dataSource = titles
+        dropDown.selectionAction = { [weak self] (index, item) in
+            self?.courseTitleButton.setTitle(item, for: .normal)
+            self?.loadTitleData()
+        }
+        dropDown.dismissMode = .automatic
+        dropDown.width = 150
+        dropDown.direction = .bottom
+//        print("Executes Also")
+        dropDown.show()
+    }
+    @objc func loadNumber() {
+        let dropDown = DropDown()
+        dropDown.anchorView = courseNumberButton
+        dropDown.dataSource = numbers
+        dropDown.selectionAction = { [weak self] (index, item) in
+            self?.courseNumberButton.setTitle(item, for: .normal)
+            self?.loadNumberData()
+        }
+        dropDown.dismissMode = .automatic
+        dropDown.width = 100
+        dropDown.direction = .bottom
+        dropDown.show()
+    }
+    @IBAction func onClickCourseTitle(_ sender: Any) {
+        loadTitleData()
+        loadTitle()
+    }
+    @IBAction func onClickCourseNumber(_ sender: Any) {
+        loadNumberData()
+        loadNumber()
+    }
+    
     @IBAction func onCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
         picker.delegate = self
